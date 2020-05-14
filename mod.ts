32be4ab572a11b3dfeb8c617cc790c3f8530c6e7
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-14 17:22:39 +0530

    :sparkles: Clone repo from git

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 import { ensureDir } from "https://deno.land/std/fs/mod.ts";
 import { join } from "https://deno.land/std/path/mod.ts";
+const { run } = Deno;
 
 const DATABASE_URL =
   "https://raw.githubusercontent.com/denoland/deno_website2/master/database.json";
@@ -14,9 +15,18 @@ export async function downloadProjects(): Promise<void> {
       desc?: string;
     };
   } = await (await fetch(DATABASE_URL)).json();
-  for await (const project of Object.keys(database)) {
-    console.log("Downloading package", project);
-    await ensureDir(join(".", "public", project));
+  await ensureDir(join(".", "public"));
+  for await (const key of Object.keys(database)) {
+    const project = database[key];
+    const DOWNLOAD_URL =
+      `https://${project.type}.com/${project.owner}/${project.repo}.git`;
+    console.log("Downloading package", key);
+    const cloner = run({
+      cmd: ["git", "clone", DOWNLOAD_URL, `public/${key}`],
+      stdout: "inherit",
+    });
+    await cloner.status();
+    break;
   }
 }
 
